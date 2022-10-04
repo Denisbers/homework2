eed782ca352c349c59679334110fbd4717d3dcba
Denisbers <denis16031984@gmail.com>
2022-10-04 23:58:46 +0300

    repo1

---
+++ app.js
@@ -0,0 +1,19 @@
+let digit1 = prompt('Enter number 1')
+
+let digit2 = prompt('Enter number 2')
+
+digit1 = parseInt(digit1)
+digit2 = parseInt(digit2)
+
+alert((digit1) + (digit2));
+console.log(alert);
+
+alert((digit1) - (digit2));
+console.log(alert);
+
+alert((digit1) * (digit2));
+console.log(alert);
+
+alert((digit1) / (digit2));
+console.log(alert);
+
